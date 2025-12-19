9d8f2db3fb0bb827695ec7890027d39042396f4e
bala-amypo <bala@amypo.in>
2025-12-19 09:29:26 +0000

    User commit

--- src/main/java/com/example/demo/security/jwt/JwtUserDetails.java
+++ src/main/java/com/example/demo/security/jwt/JwtUserDetails.java
@@ -2,7 +2,6 @@ package com.example.demo.security.jwt;
 
 import com.example.demo.model.User;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -18,9 +17,7 @@ public class JwtUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(
-                new SimpleGrantedAuthority(user.getRole())
-        );
+        return Collections.emptyList(); // no roles
     }
 
     @Override
@@ -30,11 +27,18 @@ public class JwtUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return user.getUsername();
     }
 
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return user.getActive(); }
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
