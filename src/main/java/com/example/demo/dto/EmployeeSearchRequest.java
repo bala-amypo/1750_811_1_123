5db7667071160509b6e2f18a32bf6558c4fd1a52
bala-amypo <bala@amypo.in>
2025-12-24 05:28:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/EmployeeSearchRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class EmployeeSearchRequest {
+
+    private String skill;
+    private String proficiency;
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getProficiency() {
+        return proficiency;
+    }
+
+    public void setProficiency(String proficiency) {
+        this.proficiency = proficiency;
+    }
+}
