5eb8e1d614a04544eac0d2f85d585366dca7e030
bala-amypo <bala@amypo.in>
2025-12-24 05:34:57 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRegisterRequest.java
+++ src/main/java/com/example/demo/dto/AuthRegisterRequest.java
@@ -1,23 +1,44 @@
 package com.example.demo.dto;
 
-public class SkillCategoryDTO {
+public class AuthRegisterRequest {
 
-    private Long id;
-    private String categoryName;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
 
-    public Long getId() {
-        return id;
+    public AuthRegisterRequest() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getFullName() {
+        return fullName;
     }
 
-    public String getCategoryName() {
-        return categoryName;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
