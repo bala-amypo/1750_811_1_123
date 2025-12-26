bdcf1144787f3ecb20849e5692b2b4943252a8e4
bala-amypo <bala@amypo.in>
2025-12-26 15:20:51 +0000

    User commit

--- src/test/java/com/example/demo/TestResultListener.java
+++ src/test/java/com/example/demo/TestResultListener.java
@@ -1,22 +1,20 @@
-package com.example.demo;
-
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
+ 
 public class TestResultListener implements ITestListener {
-
+ 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println(result.getName() + " - PASS");
+        System.out.println(result.getMethod().getMethodName() + " - PASS");
     }
-
+ 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println(result.getName() + " - FAIL");
+        System.out.println(result.getMethod().getMethodName() + " - FAIL");
     }
-
+ 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println(result.getName() + " - SKIPPED");
+        System.out.println(result.getMethod().getMethodName() + " - SKIP");
     }
 }
