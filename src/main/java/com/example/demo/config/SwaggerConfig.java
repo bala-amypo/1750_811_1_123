273be6826b3cec96c899c12b3ba9981e3efa934a
bala-amypo <bala@amypo.in>
2025-12-26 04:17:36 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,21 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Employee Skills Matrix API")
-                        .description("API documentation for Employee Skills Matrix System")
-                        .version("1.0.0")
-                );
-    }
+                .servers(List.of(
+                        new Server().url("https://9308.pro604cr.amypo.ai/")
+                ));
+        }
 }
